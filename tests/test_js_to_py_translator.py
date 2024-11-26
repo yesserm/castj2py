0d0fc06734fc7d5226bf8f40c6603f3c910f4659
Yesser Miranda <yesser.miranda0106@gmail.com>
2024-11-26 16:50:27 -0600

    test(samples) - add init to test and some code to samples

--- tests/test_js_to_py_translator.py
+++ tests/test_js_to_py_translator.py
@@ -1,12 +1,12 @@
 import unittest
-import src.core.js_to_py_translator
+from src.core import js_to_py_translator
 
 
 class TestJsToPyTranslator(unittest.TestCase):
     def test_translation(self):
         js_code = "document.querySelector('button').click();"
         expected_py_code = "driver.find_element_by_css_selector('button').click()"
-        self.assertEqual(src.core.js_to_py_translator.translate_js_to_py(js_code), expected_py_code)
+        self.assertEqual(js_to_py_translator.translate_js_to_py(js_code), expected_py_code)
 
 
 if __name__ == '__main__':
