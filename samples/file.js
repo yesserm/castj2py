fbbb15e6d51ea06eec3b3b734de430779fd72355
Yesser Miranda <yesser.miranda0106@gmail.com>
2024-11-28 10:58:34 -0600

    feat(core|gui) - testing conversions from js files to python using more keys

--- samples/file.js
+++ samples/file.js
@@ -1,3 +1,7 @@
 let numbers = [1, 2, 3, 4, 5];
 let doubled = numbers.map(num => num * 2);
 console.log(doubled);
+
+if(numbers.includes(3)) {
+  console.log('Numbers includes 3');
+}
--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -3,7 +3,7 @@ import os
 import sys
 import logging
 from tkinter import filedialog, messagebox
-from src.core.converter import convert_json_to_py
+from src.core.converter import convert_json_to_py, convert_js_file_to_py
 from src.utils.conversion_dict import get_project_root, ConversionDict
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -15,11 +15,14 @@ class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.json_file_path = None
+        self.js_file_path = None
         self.convert_button = None
+        self.load_js_button = None
         self.load_button = None
+        self.convert_js_button = None
         self.python_file_path = None
         self.title("JSON and JS to Python Converter")
-        self.geometry("400x200")
+        self.geometry("400x300")
 
         # Obtener el diccionario de conversión una sola vez
         db_path = os.path.join(get_project_root(), '..', 'conversion_dict.db')
@@ -31,13 +34,32 @@ class MainWindow(tk.Tk):
     def create_widgets(self):
         self.load_button = tk.Button(self, text="Load JSON File", command=self.load_json_file)
         self.load_button.pack(pady=20)
+        self.load_js_button = tk.Button(self, text="Load JS File", command=self.load_js_file)
+        self.load_js_button.pack(pady=20)
         self.convert_button = tk.Button(self, text="Convert to Python", command=self.convert_to_python)
         self.convert_button.pack(pady=20)
+        self.convert_js_button = tk.Button(self, text="Convert JS to Python", command=self.convert_js_to_python)
+        self.convert_js_button.pack(pady=20)
 
     def load_json_file(self):
         self.json_file_path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
         if self.json_file_path:
             messagebox.showinfo("File Loaded", f"Loaded {self.json_file_path}")
+
+    def load_js_file(self):
+        self.js_file_path = filedialog.askopenfilename(filetypes=[("JavaScript files", "*.js")])
+        if self.js_file_path:
+            messagebox.showinfo("File Loaded", f"Loaded {self.js_file_path}")
+
+    def convert_js_to_python(self):
+        if hasattr(self, 'js_file_path'):
+            output_file_path = filedialog.asksaveasfilename(defaultextension=".py",
+                                                            filetypes=[("Python files", "*.py")])
+            if output_file_path:
+                convert_js_file_to_py(self.js_file_path, output_file_path, self.conversion_dict)
+                messagebox.showinfo("Conversion Complete", f"Saved converted file to {output_file_path}")
+            else:
+                messagebox.showwarning("No File Loaded", "Please load a JS file first.")
 
     def convert_to_python(self):
         if hasattr(self, 'json_file_path'):
