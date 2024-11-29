53186d6ae4e8dc4bd3ec62a222a6fa1e4bb32dd0
Yesser Miranda <yesser.miranda0106@gmail.com>
2024-11-28 20:21:49 -0600

    feat(core|utils) - add support to pythonmonkey to exec js not compatible

--- samples/file.js
+++ samples/file.js
@@ -1,5 +1,7 @@
 let numbers = [1, 2, 3, 4, 5];
 
 if(numbers) {
-  console.log('existe');
+  for(let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+  }
 }
--- src/core/js_to_py_translator.py
+++ src/core/js_to_py_translator.py
@@ -1,5 +1,7 @@
 import re
 import logging
+
+import execjs
 import js2py
 
 logger = logging.getLogger('app_logger')
@@ -19,10 +21,22 @@ def translate_js_to_py(js_cod, conversion_d):
 
 
 def translate_js_to_py_js2py(js_cod):
-    file_path = js_cod
-    with open(file_path, 'r') as file:
-        js_cod = file.read()
-    logger.info(f"Translating JS to Python with JS2PY code: {js_cod}")
-    py_code = js2py.translate_js(js_cod)
-    logger.info(f"JS2PY code: {py_code}")
+    try:
+        file_path = js_cod
+        with open(file_path, 'r') as file:
+            js_cod = file.read()
+        logger.info(f"Translating JS to Python with JS2PY code: {js_cod}")
+        py_code = js2py.translate_js(js_cod)
+        logger.info(f"JS2PY code: {py_code}")
+    except js2py.base.PyJsException as e:
+        logger.error(f"Error translating JS to Python with JS2PY: {e}")
+        # use a pythonmonkey to translate the code
+        return f''' 
+        import pythonmonkey 
+        js_code = """{js_cod}""" 
+        result = pythonmonkey.run(js_code) 
+        '''
+
+    # add import execjs to the top of the file
+    py_code = 'import execjs\n' + py_code
     return py_code
